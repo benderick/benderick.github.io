8b8a92126cf3eba3b135863c9e442965b11510a9
benderick <46258069+benderick@users.noreply.github.com>
2022-08-22 18:27:00 +0800

    Update ChromeJs.js

--- ChromeJs.js
+++ ChromeJs.js
@@ -1,7 +1,7 @@
 function get_current_element(event){
         var x = event.clientX, y = event.clientY,
         element = document.elementFromPoint(x, y);
-        element.innerHTML = "<p><span class="refcolumn"><span class='refcolumn' style='background-color:#CCFFCC;'></span></span></p>"
+        element.innerHTML = "<p><span class='refcolumn'><span class='refcolumn' style='background-color:#CCFFCC;'></span></span></p>"
         window.ondblclick = null;
     }
 window.ondblclick = get_current_element;
